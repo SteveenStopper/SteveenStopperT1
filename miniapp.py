fe775e620a7c974661d46d832a1b3f371c80724d
AlexSG <alexanderstopper@hotmail.com>
2022-05-27 19:33:11 -0500

    Integrando Navbar

--- miniapp.py
+++ miniapp.py
@@ -1,16 +1,17 @@
-#importar la libreria flask
-from flask import Flask, render_template, abort
-#incializacion de la variable
+#importarmas la libreria flask
+from flask import Flask, render_template
+
+#incializamos la variable
 app = Flask(__name__, template_folder= 'templates')
 
 
 #ruta - Página principal
 @app.route('/')
 def pag_principal():
-    return render_template('index.html')
+    return render_template('main.html')
 
 
 #main del programa
 if __name__ == '__main__':
-    #debug = True, para reiniciar automáticamente el servidor
+    #debug = True, para reiniciar automáticamente el servidor.
     app.run(debug=True)
